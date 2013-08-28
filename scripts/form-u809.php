7ad96e48ad12119e56c61add1f7d6683b07c3797
Luke McKinney <mckinneymakesmedia@gmail.com>
2013-08-28 16:17:44 -0500

    updated contact form
    changed contact form to luke@mckinneymakesmedia.com

--- scripts/form-u809.php
+++ scripts/form-u809.php
@@ -20,7 +20,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 
 function emailFormSubmission()
 {
-	$to = 'mckinneymakesmedia@gmail.com';
+	$to = 'luke@mckinneymakesmedia.com';
 	$subject = 'about me/ contact Form Submission';
 	
 	$message = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/><title>' . htmlentities($subject,ENT_COMPAT | ENT_HTML401,'UTF-8') . '</title></head>';
@@ -40,7 +40,7 @@ function emailFormSubmission()
 	$message = cleanupMessage($message);
 	
 	$formEmail = cleanupEmail($_REQUEST['Email']);
-	$headers = 'From:  mckinneymakesmedia@gmail.com' . "\r\n" . 'Reply-To: ' . $formEmail .  "\r\n" .'X-Mailer: Adobe Muse 5.0.704 with PHP/' . phpversion() . "\r\n" . 'Content-type: text/html; charset=utf-8' . "\r\n";
+	$headers = 'From:  luke@mckinneymakesmedia.com' . "\r\n" . 'Reply-To: ' . $formEmail .  "\r\n" .'X-Mailer: Adobe Muse 5.0.704 with PHP/' . phpversion() . "\r\n" . 'Content-type: text/html; charset=utf-8' . "\r\n";
 	
 	$sent = @mail($to, $subject, $message, $headers);
 	
